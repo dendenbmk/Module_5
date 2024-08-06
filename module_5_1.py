c939fca1e8ab16a8002dfe67a7981e8da3c879ae
dendenbmk <denden_bmk@rambler.ru>
2024-08-06 11:29:21 +0300

    Update module_5_1.py

--- module_5_1.py
+++ module_5_1.py
@@ -5,15 +5,15 @@ class House:                                         # Создали Kласс 
 
 
     def go_to(self, namber_of_want_floor):       # Создали метод go_to, куда будем передовать кол-во этажей при вызове
-        self.number_of_floors                    # сделали доступным глобальный атрибут   number_of_floors
         if namber_of_want_floor > self.number_of_floors or namber_of_want_floor < 1:  # если передадим желаемый этаж
                                                    # больше чем number_of_floors или меньше 1, то ...
             print('Такого этажа не существует')    #  будет выводиться это сообщение
         else:                                      # иначе...
-            print(namber_of_want_floor)            # выведим желаемый этаж
+            for i in range(namber_of_want_floor + 1): # перебераем числа (этажы) от 1 до нужного
+                print(i)                                     # и выводим в консоль
 
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
-h1.go_to(20)
-h2.go_to(2)
+h1.go_to(5)
+h2.go_to(10)
